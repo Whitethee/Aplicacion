2c75776b3db72ea788ffad0993c017847bb923ce
egrache <evgeniygrachev@icloud.com>
2024-05-17 17:35:29 +0100

    Update Modelo.py

--- pages/Modelo.py
+++ pages/Modelo.py
@@ -89,8 +89,8 @@ def surveyMod():
         #results = pd.DataFrame(survey_results)
         results = pd.DataFrame([survey_results])
         st.write(results)
-        for i in data:
-            data = data[i].replace(mappings, inplace= True)
+        for i, value in data.iteritems():
+            data = value.replace(mappings, inplace= True)
             st.write(data)
         data = results.replace(mappings, inplace=True)
         st.write(data)
