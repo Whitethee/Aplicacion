608007cb7804c5c25661c15ed744424e63b47f73
egrache <evgeniygrachev@icloud.com>
2024-05-20 16:46:43 +0100

    Update Modelo.py

--- pages/Modelo.py
+++ pages/Modelo.py
@@ -139,7 +139,7 @@ def surveyMod():
                     font-size:20px !important;  # Hace el prefijo más pequeño
                     }
                     </style>""", unsafe_allow_html=True)
-                st.markdown(f'<p class="small-font">La duración de la intervencion sera de:  </p><p class="big-font">{res} min</p>', unsafe_allow_html=True)
+                st.markdown(f'<p class="small-font">La duración estimada de la intervencion sera de:  </p><p class="big-font">{res} min</p>', unsafe_allow_html=True)
 
             with c2:
                 st.image("verdecirc.jpg", use_column_width="auto", output_format="PNG")
